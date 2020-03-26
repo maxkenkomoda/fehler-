b66d55bbfd0a51919ed9d998e87774587a046d54
moko.eng <tec.lesson.kk@gmail.com>
2020-03-26 10:22:17 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
   
+  #ログインのルーティング
+  get 'login',to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  #会員登録のルーティン
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
 end
